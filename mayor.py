e27c170199e21773bb54fea15809db1a40b65604
Escobar Silva <000404669@bga.upb>
2019-03-01 14:00:24 -0500

    Ejercicio resuelto

--- mayor.py
+++ mayor.py
@@ -1,6 +1,30 @@
-print("Primer ejercicio")
-a=int(input("Ingrese el número deseado: "))
-if a>0:
-    print("El número es mayor a cero")
-else:
-    print("El número no es mayor que cero")
+
+contadorPos=0
+contadorNeg=0
+contadorCeros=0
+
+
+for x in range(1,11):
+    x=int(input("Ingrese el número: "))
+    if x>0:
+        contadorPos=contadorPos+1
+    elif x<0:
+        contadorNeg=contadorNeg+1
+
+    else:
+        contadorCeros=contadorCeros+1
+
+print("Los número positivos son", contadorPos)
+print("Los números negativos son", contadorNeg)
+print("Hay",contadorCeros,"ceros")
+
+
+
+
+
+
+
+
+
+
+
